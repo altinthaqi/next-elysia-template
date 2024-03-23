eee5f18958ffb1d3e5a93050704d2ad24c7d990e
Altin Thaci <altin.thaci@fresha.com>
2024-03-23 17:13:05 +0100

    Add Eden treaty, run Elysia on Next's route

---
+++ app/libs/api.ts
@@ -0,0 +1,8 @@
+import { treaty } from "@elysiajs/eden";
+import { API } from "../api";
+
+export const api = treaty<API>(
+  typeof window === "undefined"
+    ? `http://localhost:${process.env.PORT ?? 3000}`
+    : window.location.origin
+).api;
